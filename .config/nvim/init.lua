98855292efb9554f5f92c2f889b1b2fc57c3414d
saltchicken <john.eicher89@gmail.com>
2025-04-18 14:29:01 -0700

    Add hotkeys for augment

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.keymap.set("n", "<leader>ac", ":Augment chat<CR>", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>ac", ":Augment chat<CR>", { noremap = true, silent = true })
+
+vim.g.augment_workspace_folders = { "~/workspace" }
