1e3144959780902c9319f3f2a8b0418632b94b11
saltchicken <john.eicher89@gmail.com>
2025-04-28 22:02:41 -0700

    Change transparency of beacon

--- .config/nvim/lua/plugins/custom.lua
+++ .config/nvim/lua/plugins/custom.lua
@@ -39,8 +39,8 @@ return {
     opts = {
       enabled = true, --- (boolean | fun():boolean) check if enabled
       speed = 2, --- integer speed at wich animation goes
-      width = 40, --- integer width of the beacon window
-      winblend = 70, --- integer starting transparency of beacon window :h winblend
+      width = 20, --- integer width of the beacon window
+      winblend = 95, --- integer starting transparency of beacon window :h winblend
       fps = 60, --- integer how smooth the animation going to be
       min_jump = 10, --- integer what is considered a jump. Number of lines
       cursor_events = { "CursorMoved" }, -- table<string> what events trigger check for cursor moves
