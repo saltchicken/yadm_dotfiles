375cb9cb9c5dd86f67130c55a6dba952c6f1dc93
saltchicken <john.eicher89@gmail.com>
2025-07-13 10:42:36 -0700

    Add popup messaging scripts

---
+++ .config/tmux/scripts/popup_neovim.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+TMPFILE=$(mktemp)
+trap 'rm -f "$TMPFILE"' EXIT
+
+nvim "$TMPFILE" >/dev/tty </dev/tty
+
+CONTENT=$(cat "$TMPFILE")
+
+OUTPUT=$(/home/saltchicken/.config/tmux/scripts/process_input.sh "$CONTENT")
+
+OUTPUT_TMPFILE=$(mktemp)
+trap 'rm -f "$OUTPUT_TMPFILE"' EXIT
+
+echo "$OUTPUT" >"$OUTPUT_TMPFILE"
+
+sleep 4
+
+nvim "$OUTPUT_TMPFILE" >/dev/tty </dev/tty
---
+++ .config/tmux/scripts/process_input.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Simple script to process input and return it.
+# The first argument ($1) will be the text entered in the tmux prompt.
+# gemini --prompt "$1"
+echo "$1"
