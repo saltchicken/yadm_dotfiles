2efb76de7eedebc9aa1602311fb203ff9bf39ca1
saltchicken <john.eicher89@gmail.com>
2025-09-12 08:53:05 -0700

    Set keymaps in keymaps

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,25 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+-- vim.g.augment_workspace_folders = { "~/workspace/rust_sandbox/" }
 
 vim.opt.laststatus = 0
-
-vim.keymap.set("n", "<leader>ac", ":Augment chat<CR>", { noremap = true, silent = true })
-vim.keymap.set("v", "<leader>ac", ":Augment chat<CR>", { noremap = true, silent = true })
-
-vim.keymap.set("n", "<leader>at", ":Augment chat-toggle<CR>", { noremap = true, silent = true })
-vim.keymap.set("v", "<leader>at", ":Augment chat-toggle<CR>", { noremap = true, silent = true })
-
-vim.g.augment_workspace_folders = { "~/.config/tmux/" }
-
-vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", {})
-vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", {})
-vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "[B]uffer [D]elete" })
-
-vim.keymap.set("n", "<leader>hp", function()
-  Snacks.dashboard()
-end, { desc = "Show Dashboard" })
-
-vim.keymap.set("n", "<leader>db", ":DBUIToggle<CR>", { desc = "Open DBUI" })
 
 vim.opt.clipboard = "unnamedplus"
 
--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,15 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+vim.keymap.set("n", "<leader>ac", ":Augment chat<CR>", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>ac", ":Augment chat<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>at", ":Augment chat-toggle<CR>", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>at", ":Augment chat-toggle<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<Tab>", "<cmd>BufferLineCycleNext<CR>", {})
+vim.keymap.set("n", "<S-Tab>", "<cmd>BufferLineCyclePrev<CR>", {})
+vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<CR>", { desc = "[B]uffer [D]elete" })
+
+vim.keymap.set("n", "<leader>hp", function()
+  Snacks.dashboard()
+end, { desc = "Show Dashboard" })
+
+vim.keymap.set("n", "<leader>db", ":DBUIToggle<CR>", { desc = "Open DBUI" })
