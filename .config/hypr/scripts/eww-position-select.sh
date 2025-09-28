3725a5a45de03dd5e7c3588856998901acfb3d4c
saltchicken <john.eicher89@gmail.com>
2025-09-28 14:10:30 -0700

    Remove old scripts

--- .config/hypr/scripts/eww-position-select.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-hyprctl monitors -j | jq -r '
-    .[] 
-    | select(.focused) 
-    | "hyprctl dispatch movecursor \((.x + (.width / 2)) | round) \((.y + (.height / 2)) | round)"
-' | sh
-eww open position_hud
--- .config/hypr/scripts/position-window.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-pos=$(echo "center\ntop-left\nlower-left\ntop-right\nlower-right" | wofi --dmenu --prompt "Select position:")
-case "$pos" in
-"top-left") hyprctl --batch "dispatch setfloating 1; dispatch resizeactive exact 1280 720; dispatch moveactive exact 0 0" ;;
-"lower-left") hyprctl --batch "dispatch setfloating 1; dispatch resizeactive exact 1280 720; dispatch moveactive exact 0 720" ;;
-"top-right") hyprctl --batch "dispatch setfloating 1; dispatch resizeactive exact 1280 720; dispatch moveactive exact 3840 0" ;;
-"lower-right") hyprctl --batch "dispatch setfloating 1; dispatch resizeactive exact 1280 720; dispatch moveactive exact 3840 720" ;;
-"center") hyprctl --batch "dispatch setfloating 1; dispatch resizeactive exact 2560 1440; dispatch moveactive exact 1280 0" ;;
-esac
