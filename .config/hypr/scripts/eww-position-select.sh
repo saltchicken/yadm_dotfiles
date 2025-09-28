740cf058ebb42d9229123b6e0daa6450caf00ea4
saltchicken <john.eicher89@gmail.com>
2025-09-27 18:46:44 -0700

    Center the cursor

--- .config/hypr/scripts/eww-position-select.sh
+++ .config/hypr/scripts/eww-position-select.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+hyprctl monitors -j | jq -r '
+    .[] 
+    | select(.focused) 
+    | "hyprctl dispatch movecursor \((.x + (.width / 2)) | round) \((.y + (.height / 2)) | round)"
+' | sh
 eww open position_hud
